7127330111cae8cf43d46c8b353b5ffbb52f54aa
mikeblank1 <mikeblank1@gmail.com>
2024-05-02 12:44:10 -0700

    Update index.tsx

--- ui/rte/src/rte_config/DamEnv/index.tsx
+++ ui/rte/src/rte_config/DamEnv/index.tsx
@@ -5,9 +5,9 @@ const DamEnvVariables = {
   ASSET_NAME_PARAM: "assetId",
   RTE_DAM_ICON: (
     <svg
-      width="20"
-      height="20"
-      viewBox="0 0 20 20"
+      width="1024"
+      height="1024"
+      viewBox="0 0 1024 1024"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
