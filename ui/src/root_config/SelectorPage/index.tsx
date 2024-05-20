46c2195ca80a45c5a14d6dc921a64209a23c69d6
mikeblank1 <mikeblank1@gmail.com>
2024-05-20 07:00:00 -0700

    Update index.tsx

--- ui/src/root_config/SelectorPage/index.tsx
+++ ui/src/root_config/SelectorPage/index.tsx
@@ -64,18 +64,20 @@ const openComptactView = (
               fetch("https://author-p131626-e1282650.adobeaemcloud.com/adobe/repository/content/dam/wegmans/marketing/assets/products/recipes/web_recipes/RECIPE_13362.jpg;api=block_download;renditionName=cq5dam.web.1280.1280.jpeg;version=oak%3A1.0%3A%3Aci%3Aff0bd018623b3ac9fbd87c90fe866e84;t=1716").then(res => res.json()).then(out =>
                 console.log('Checkout this JSON! ', out))
                 .catch(err => { console.log(err)});
-              //alert("success");
+              alert("success");
               const xhr = new XMLHttpRequest();
               xhr.open("GET", ("https://author-p131626-e1282650.adobeaemcloud.com/adobe/repository/content/dam/wegmans/marketing/assets/products/recipes/web_recipes/RECIPE_13362.jpg;api=block_download;renditionName=cq5dam.web.1280.1280.jpeg;version=oak%3A1.0%3A%3Aci%3Aff0bd018623b3ac9fbd87c90fe866e84;t=1716"));
               xhr.send();
               xhr.responseType = "json";
               xhr.onload = () => {
+                /*jslint eqeq: true*/
                 if (xhr.readyState == 4 && xhr.status == 200) {
                   console.log(xhr.response);
                 } else {
                   console.log(`Error: ${xhr.status}`);
                 }
               };
+              alert("success 2");
 
               //onSuccess(formattedAssets);
             } else {
